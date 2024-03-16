8ef13ac7cd7ee64db98be1ead2e49c68ebbafaa3
SergWebAcceptance <serg.web@acceptance.ltd>
2024-03-16 19:48:20 +0200

    fixed state

--- src/components/insights/InsightsSecond.jsx
+++ src/components/insights/InsightsSecond.jsx
@@ -1,5 +1,7 @@
+'use client';
 import { getInsights } from "@/lib/insights";
 import InsightCard from "./InsightCard";
+import { useState, useEffect } from "react";
 
 export default async function InsightsSecond() {
   const [insights, setInsights] = useState([]);
