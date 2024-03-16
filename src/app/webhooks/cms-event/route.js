bd11f765cab540ed7c109c8ef6079b9a48d066c3
SergWebAcceptance <serg.web@acceptance.ltd>
2024-03-16 19:16:05 +0200

    Tested revalidation

--- src/app/webhooks/cms-event/route.js
+++ src/app/webhooks/cms-event/route.js
@@ -4,6 +4,9 @@ import { revalidateTag } from "next/cache";
 export async function POST(request) {
     const payload = await request.json();
     console.log('payload:', payload);
-    revalidateTag(CACHE_TAG_REVIEWS);
+    if(payload_type == 'insights') {
+        revalidateTag(CACHE_TAG_REVIEWS);
+        console.log('Tag revalidated');
+    }
     return new Response(null, { status: 204 })
 }
