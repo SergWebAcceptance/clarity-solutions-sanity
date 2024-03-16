53ddcc38c0203bb08949ec30517c032ba6806881
SergWebAcceptance <serg.web@acceptance.ltd>
2024-03-16 19:58:10 +0200

    fixed insights

--- src/components/insights/InsightsSecond.jsx
+++ src/components/insights/InsightsSecond.jsx
@@ -1,19 +1,8 @@
-'use client';
 import { getInsights } from "@/lib/insights";
 import InsightCard from "./InsightCard";
-import React, { useEffect, useState } from 'react';
 
-export default function InsightsSecond() {
-  const [insights, setInsights] = useState([]);
-
-  useEffect(() => {
-    async function fetchInsights() {
-      const fetchedInsights = await getInsights();
-      setInsights(fetchedInsights);
-    }
-
-    fetchInsights();
-  }, []);
+export default async function InsightsSecond() {
+  const insights = await getInsights();
   return (
     <>
       <section className="insights-wrap">
